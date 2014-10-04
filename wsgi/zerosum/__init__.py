6522a20c7d0637350a2c2ea3e5a819a2cb552972
Karl Bartel <karl42@gmail.com>
2014-10-04 17:56:28 +0200

    send error mails on 500

--- wsgi/zerosum/__init__.py
+++ wsgi/zerosum/__init__.py
@@ -4,17 +4,29 @@ from decimal import Decimal
 from flask import Flask, render_template, redirect, request, url_for, flash
 from flask.ext.login import login_required, current_user
 
+ADMINS = ['karl42@gmail.com']
+
 app = Flask(__name__.split('.')[0])
 app.debug = os.environ.get('FLASK_DEBUG', 'false').lower() == 'true'
 if not app.debug:
     import logging
+
     from logging.handlers import RotatingFileHandler
     file_handler = RotatingFileHandler(
         os.environ['OPENSHIFT_LOG_DIR'] + '/flask.log',
         maxBytes=10000, backupCount=1)
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
-    print('added log handler')
+
+    from logging.handlers import SMTPHandler
+    credentials = (os.environ.get('SENDGRID_USER'),
+                   os.environ.get('SENDGRID_PASSWORD'))
+    mail_handler = SMTPHandler('smtp.sendgrid.net',
+                               'server-error@trackmyowe.com',
+                               ADMINS, 'TrackMyOwe.com Error',
+                               credentials)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
 
 from zerosum.db import get_db, get_scalar, get_all
 import zerosum.owe
